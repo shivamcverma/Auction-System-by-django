50f2744f3e057099489a56b17f62422c5bb7ee06
Achyutashivam <shivam@achyutagroup.com>
2026-04-08 11:14:05 +0530

    few

--- AuctionSystem/settings.py
+++ AuctionSystem/settings.py
@@ -125,6 +125,7 @@ import os
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ALLOWED_HOSTS = ['auction-system-by-django.onrender.com']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
